e7ffbc6f46745bfc2901c2ec0c77c0b8988eaa7f
HorstDev <gorstsergei@mail.ru>
2025-01-03 22:21:03 +0400

    Добавлена инициализация администратора в БД

---
+++ src/main/java/org/astu/attendancetracker/persistence/DataLoader.java
@@ -0,0 +1,35 @@
+package org.astu.attendancetracker.persistence;
+
+import org.astu.attendancetracker.core.domain.Role;
+import org.astu.attendancetracker.core.domain.User;
+import org.astu.attendancetracker.persistence.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+
+    public DataLoader(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.saveAll(users());
+    }
+
+    private List<User> users() {
+        User admin = User
+                .builder()
+                .login("admin_astu")
+                .password("$2a$10$unePMYhEhAvadfyPIy/H5uXExdCgAhj46j0Q2gij8WugLy37CrQni")
+                .role(Role.ADMIN)
+                .build();
+
+        return List.of(admin);
+    }
+}
